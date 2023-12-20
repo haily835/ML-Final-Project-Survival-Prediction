e23ae1423a27beeda24b76f1132079c929a25dbd
Nhan <nguyen6n@uwindsor.ca>
2023-12-20 16:27:08 -0500

    add classifier

--- k_fold.py
+++ k_fold.py
@@ -4,13 +4,14 @@ from sklearn.model_selection import StratifiedKFold
 from imblearn.over_sampling import SMOTE
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score
 
-
-def k_fold(X, y, classifier, k_folds=10):
+def k_fold(X, y, classifier, k_folds=10, verbose=True):
     skf = StratifiedKFold(n_splits=k_folds, shuffle=True, random_state=42)
     smote = SMOTE(random_state=42)
 
     weighted_accuracies = []
+    f1_scores = []
 
     i = 0
     for train_index, test_index in skf.split(X, y):
@@ -27,13 +28,22 @@ def k_fold(X, y, classifier, k_folds=10):
         y_pred = classifier.predict(X_test)
 
         # Calculate and store the weighted accuracy
-        weighted_accuracy = accuracy_score(y_test, y_pred, sample_weight=None)  # You may need to adjust the sample_weight parameter
+        weighted_accuracy = accuracy_score(y_test, y_pred, sample_weight=None)
         weighted_accuracies.append(weighted_accuracy)
-        print(f"Fold {i+1}: {weighted_accuracy}")
-        i += 1
-        
 
-    # Calculate the average weighted accuracy
+        # Calculate and store the F1 score
+        f1 = f1_score(y_test, y_pred, average='weighted')
+        f1_scores.append(f1)
+        if verbose:
+            print(f"Fold {i+1} - Weighted Accuracy: {weighted_accuracy} - F1 Score: {f1}")
+        
+        i += 1
+
+    # Calculate the average weighted accuracy and F1 score
     average_weighted_accuracy = np.mean(weighted_accuracies)
-    print("Average Weighted Accuracy:", average_weighted_accuracy)
-    return average_weighted_accuracy
+    average_f1_score = np.mean(f1_scores)
+
+    print(f"Classifier: {classifier.__class__.__name__}")
+    print(f"Avg Weighted Accuracy: {average_weighted_accuracy}")
+    print(f"Avg F1 Score: {average_f1_score}\n")
+    return average_weighted_accuracy, average_f1_score
